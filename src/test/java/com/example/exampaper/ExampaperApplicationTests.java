0b8c9c415c5e4057d52e91dbee835e898f90c7fb
fluency <457679605@qq.com>
2022-07-23 20:51:07 +0800

    工程初始化

---
+++ src/test/java/com/example/exampaper/ExampaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exampaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampaperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
